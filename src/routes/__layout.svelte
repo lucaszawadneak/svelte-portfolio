d117a7d1e45165ddc30fefb392aab455206737ed
Lucas Cassilha <lucaszawadneak@gmail.com>
2022-06-27 17:30:23 -0300

    feat:initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "../app.css";
+
+</script>
+
+<div class="bg-gray-900 h-screen text-white p-2">
+  <nav class="w-full">
+    <a href="/">Home</a>
+    <a href="/projects">Projects</a>
+    <a href="/projects">Education</a>
+  </nav>
+
+  <slot />
+</div>
---
+++ src/routes/projects/index.svelte
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<div>
+  <h1>projects</h1>
+</div>
